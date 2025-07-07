a6f0baa3bffaa0a131283d27eb393f49cb12dfbf
Shahmeer1504 <202447014+ShahmeerAli1504@users.noreply.github.com>
2025-07-08 01:31:23 +0500

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p className="fade-in">
-        I'm Shahmeer Ali, a Computer Science student at FAST NUCES Islamabad. I'm passionate about frontend development, game design, blockchain systems, and immersive tech like AR.
+        I'm Shahmeer Ali, a Computer Science Graduate from FAST NUCES Islamabad. I'm passionate about Frontend Development, Game Design, Blockchain Systems, and immersive tech like AR.
       </p>
       <p className="fade-in delay-1">
         I enjoy combining design with logic to build apps that are interactive and intelligent. I've interned at NADRA and Hexler Tech, and served as a TA at FAST, helping students with OS, Networks, and Data Structures.
